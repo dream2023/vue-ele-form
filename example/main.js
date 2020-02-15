fb9534ee98dde71c0e42d13ecf2056f3386a2e5a
zhangchaojie <1098626505@qq.com>
2020-02-15 21:30:20 +0800

    chore: demo版本更改

--- example/main.js
+++ example/main.js
@@ -7,7 +7,7 @@ Vue.use(Link)
 Vue.use(VueRunSfc, {
   jsLabs: [
     'https://unpkg.com/element-ui@2.13.0/lib/index.js',
-    'https://unpkg.com/vue-ele-form@0.8.1/dist/vue-ele-form.umd.min.js'
+    'https://unpkg.com/vue-ele-form'
   ],
   cssLabs: ['https://unpkg.com/element-ui/lib/theme-chalk/index.css']
 })
