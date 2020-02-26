a36b1bd4291d09025973f8c4ab27f6a9c83db3a5
zhangchaojie <1098626505@qq.com>
2020-02-26 21:39:35 +0800

    chore: 更新types定义

--- types/index.d.ts
+++ types/index.d.ts
@@ -4,8 +4,72 @@ declare module 'vue-ele-form' {
     [key: string]: any;
     public install: PluginFunction<{}>;
   }
+  interface AnyObj {
+    [key: string]: any;
+    [key: number]: any;
+  }
+
+  // 任意函数
+  interface AnyFunction {
+    (...args: any[]): any;
+  }
+
+  interface LinkageFunction {
+    (data: AnyObj): boolean;
+  }
+
+  // 格式化函数
+  interface FormatterFunction {
+    (val: any, data: AnyObj): any;
+  }
+
+  // options参数
+  type ArrayOptions = Array<string | number | boolean | object>;
+  type FunctionOptions = (
+    data: AnyObj
+  ) => ArrayOptions | Promise<any> | ArrayOptions;
+
+  export interface FormDescData {
+    type: string | LinkageFunction;
+    label?: string;
+    default?: any;
+    attrs?: AnyObj;
+    title?: string;
+    isShowLabel?: boolean;
+    labelWidth?: string;
+    isTypeChangeReloadValue?: boolean;
+    layout?: number;
+    tip?: string;
+    vif?: boolean | LinkageFunction;
+    disabled?: boolean | LinkageFunction;
+    children?: FormDesc;
+    isReloadOptions?: boolean;
+    options?: ArrayOptions | FunctionOptions | PromiseConstructor;
+    prop?: {
+      text: string;
+      value: string;
+    };
+    required?: boolean;
+    rules?: AnyObj | any[];
+    displayFormatter?: FormatterFunction;
+    valueFormatter?: FormatterFunction;
+    class?: string | string[] | object;
+    style?: string | string[] | object;
+    on?: {
+      [key: string]: AnyFunction;
+    };
+    slots?: {
+      [key: string]: AnyFunction;
+    };
+    scopedSlots?: {
+      [key: string]: AnyFunction;
+    };
+  }
+
+  export interface FormDesc {
+    [key: string]: FormDescData;
+  }
 
   const VuePlugin: EleForm;
-
   export default VuePlugin;
 }
