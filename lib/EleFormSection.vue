60d79ca7cb5ce8b021aca0223eb0627bbc7c3001
zhangchaojie <1098626505@qq.com>
2020-03-18 20:24:05 +0800

    fix: ele-form-section remote 无法设置

--- lib/EleFormSection.vue
+++ lib/EleFormSection.vue
@@ -41,45 +41,45 @@
           </slot>
           <div class="ele-form-section-body">
             <el-row :gutter="20">
-              <template v-for="(formItem, field) of section.formDesc">
+              <template v-for="(v, field) of section.formDesc">
                 <slot
                   :name="field + '-wrapper'"
                   :data="formData[field]"
-                  :desc="formItem"
+                  :desc="formDesc[field]"
                   :field="field"
                   :props="props"
                   :formData="formData"
-                  :disabled="formItem._disabled"
-                  :type="formItem._type"
-                  :options="formItem._options"
+                  :disabled="formDesc[field]._disabled"
+                  :type="formDesc[field]._type"
+                  :options="formDesc[field]._options"
                 >
                   <el-col
                     :key="field"
-                    v-bind="formItem._colAttrs"
-                    v-if="formItem._vif"
+                    v-bind="formDesc[field]._colAttrs"
+                    v-if="formDesc[field]._vif"
                   >
                     <el-form-item
                       :error="formErrorObj ? formErrorObj[field] : null"
-                      :label="props.isShowLabel ? formItem.label : null"
+                      :label="props.isShowLabel ? formDesc[field].label : null"
                       :prop="field"
                     >
                       <!-- 具名 作用域插槽(用于用户自定义显示) -->
                       <slot
                         ::data="formData[field]"
-                        :desc="formItem"
+                        :desc="formDesc[field]"
                         :props="props"
                         :field="field"
                         :formData="formData"
                         :name="field"
-                        :disabled="formItem._disabled"
-                        :type="formItem._type"
-                        :options="formItem._options"
+                        :disabled="formDesc[field]._disabled"
+                        :type="formDesc[field]._type"
+                        :options="formDesc[field]._options"
                       >
                         <component
-                          :disabled="formItem._disabled"
-                          :desc="formItem"
-                          :is="formItem._type"
-                          :options="formItem._options"
+                          :disabled="formDesc[field]._disabled"
+                          :desc="formDesc[field]"
+                          :is="formDesc[field]._type"
+                          :options="formDesc[field]._options"
                           :ref="field"
                           :readonly="props.readonly"
                           :field="field"
@@ -89,8 +89,8 @@
                       </slot>
                       <div
                         class="ele-form-tip"
-                        v-if="formItem._tip"
-                        v-html="formItem._tip"
+                        v-if="formDesc[field]._tip"
+                        v-html="formDesc[field]._tip"
                       ></div>
                     </el-form-item>
                   </el-col>
