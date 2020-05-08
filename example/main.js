12aee12ef9497248725c7f7f2f8e2f9d01bab158
zhangchaojie <1098626505@qq.com>
2020-05-08 22:01:29 +0800

    fix: 修复若干小BUG

--- example/main.js
+++ example/main.js
@@ -6,7 +6,7 @@ import { Link } from 'element-ui'
 Vue.use(Link)
 Vue.use(VueRunSfc, {
   jsLabs: [
-    'https://unpkg.com/element-ui@2.13.0/lib/index.js',
+    'https://unpkg.com/element-ui@2.13.1/lib/index.js',
     'https://unpkg.com/vue-ele-form'
   ],
   cssLabs: ['https://unpkg.com/element-ui/lib/theme-chalk/index.css']
